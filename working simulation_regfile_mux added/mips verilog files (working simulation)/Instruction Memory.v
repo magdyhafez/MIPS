7f1257cac76c4e836dde51fbdb769b3d21a5d88b
Mohamed Ismail <mihafez.mi@gmail.com>
2015-12-27 21:15:36 +0200

    last version that will be delivered tomorrow

--- working simulation_regfile_mux added/mips verilog files (working simulation)/Instruction Memory.v
+++ working simulation_regfile_mux added/mips verilog files (working simulation)/Instruction Memory.v
@@ -4,7 +4,7 @@ input [31:0] Read_Address ;     //The input to the whole module   32 bits
 output [31:0]Instruction ;     // The output of the whole module  32 bits  
  reg [31:0]Instruction ;       
 reg [31:0] instruction_memory[39:0];       // Instanciating a memory which will contain the instructions ist size width 32 bits and depth is 40  
-//reg	[31:0] imoo [255:0];
+
    
 initial
   begin
